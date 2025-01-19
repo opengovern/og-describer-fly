3523fd0f647e699e384e8d52a819a89ca45d88a2
unknown <arshia.bahar@aut.ac.ir>
2025-01-19 04:30:07 +0330

    fix: fixed some names

--- global/nats.go
+++ global/nats.go
@@ -3,7 +3,7 @@ package global
 const (
 	StreamName           = "og_describer_fly"
 	JobQueueTopic        = "og_describer_fly_job_queue"
-	ConsumerGroup        = "describers-fly"
+	ConsumerGroup        = "describer-fly"
 	JobQueueTopicManuals = "og_describer_fly_manuals_job_queue"
-	ConsumerGroupManuals = "describers-fly-manuals"
+	ConsumerGroupManuals = "describer-fly-manuals"
 )
